cae6ddea0542634e1f3738c318f4280419315aae
AIR SHOW <vishak.gowda@zysk.in>
2019-04-01 10:02:55 +0530

    Changes the animations

--- icharge/js/index.js
+++ icharge/js/index.js
@@ -1,33 +1,15 @@
-$(document).ready(function () {
-	if ($('.navbar').hasClass('trans-navbar')) {
-		// Function to make transparent navbar to white background navbar on scrolling
-		$(window).scroll(function () {
-			if 	($(document).scrollTop() > 250) {
-				$('.fixed-top').removeClass('bg-transparent').addClass('bg-white shadow-sm')
-				$('.nav-link').removeClass('text-white')
-				$('.fas.fa-bars').addClass('text-dark');
-				// $('.navbar .nav-link, .fas.fa-bars').addClass('text-dark');
-			} else {
-				$('.fixed-top').removeClass('bg-white shadow-sm').addClass('bg-transparent');
-				$('.nav-link').addClass('.active-color')
-				$('.active .nav-link').removeClass('.active-color')
-				$('.fas.fa-bars').removeClass('text-dark');
-				// $('.navbar .nav-link, .fas.fa-bars').removeClass('text-dark');
-			}
-		});
+$(window).resize(function () {
+    autoHeightMobile();
+  });
+  autoHeightMobile();
 
-		// Function to make transparent navbar to white background in mobile view on clicking toggler
-		$('.navbar-toggler').click(function () {
-			if ($('.navbar-collapse.collapse').hasClass('show') && $(document).scrollTop() < 150) {
-				// $('.fixed-top').removeClass('bg-transparent').addClass('bg-white shadow-sm');
-				$('.fixed-top').removeClass('bg-dark shadow-sm').addClass('bg-transparent');
-				$('.navbar .nav-link, .fas.fa-bars').removeClass('text-dark');
-			} else {
-				$('.fixed-top').removeClass('bg-transparent').addClass('bg-dark shadow-sm');
-				$('.fas.fa-bars').addClass('text-dark');
-				$('.navbar .nav-link').removeClass('text-dark')
-				$('.active .nav-link').removeClass('text-white')
-			}
-		});
-	}
-});
+function autoHeightMobile() {
+  var windowWidth = $(window).width();
+  if (windowWidth < 900) {
+    $('section.section').addClass('fp-auto-height');
+    // scrollOverflow = false;
+  } else {
+    $('section.section').removeClass('fp-auto-height');
+  }
+}
+
